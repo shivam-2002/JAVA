3d89af3b598df8f3bde31acd889297a4289093dc
Shivam Kumar <56464795+shivam-2002@users.noreply.github.com>
2020-10-29 09:20:19 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,60 @@
+import turtle
+'''
+w=turtle.Screen()
+w.bgcolor("pink")
+w.title("HEllo")
+w.bgpic("m3.gif")
+t=turtle.Turtle()
+t.forward(100)
+t.backward(100)
+t.reset()
+#help(turtle.hideturtle())
+t.hideturtle()
+t.circle(100)
+t.circle(100,180)
+t.circle(radius=100,steps=3)
+t.forward(100)
+t.circle(100,steps=7)
+help(turtle.home())'''
+
+'''
+t=turtle.Turtle()
+
+k=["red","blue","green","cyan","pink","brown"]
+for i in range(200):
+	t.color(k[i%5])
+	t.forward(i)
+	t.pensize(i/10+1)
+	t.left(58)
+
+	'''
+
+'''
+w=turtle.Screen()
+w.title("MY HEART")
+w.bgcolor("black")
+h=turtle.Turtle()
+h.begin_fill()
+h.color("red")
+h.left(140)
+h.forward(180)
+h.circle(-90,200)
+#h.left(120)
+h.setheading(60)
+h.circle(-90,200)
+h.forward(180)
+h.end_fill()
+'''
+
+
+e=turtle.Turtle()
+k=turtle.Screen()
+k.bgcolor("pink")
+list1=["red","cyan","blue","green","yellow"]
+for i in range(400):
+	e.forward(i)
+	e.color(list1[i%4])
+	e.right(89)
+	e.pensize(1)
+
+	
